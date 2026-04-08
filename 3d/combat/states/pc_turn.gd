9f8ae41bb23afab5b22f6050fc0659e659d003d9
Valerie Walton <57420333+RileyPersonne@users.noreply.github.com>
2026-04-08 08:56:01 -0700

    started map gen

--- 3d/combat/states/pc_turn.gd
+++ 3d/combat/states/pc_turn.gd
@@ -12,9 +12,11 @@ var turn : int
 func handle_input(_event: InputEvent) -> void:
 	if _event.is_action_pressed("click"):
 		if map.get_current_tile().t_name == "available":
+			map.map[character.data.pos.x][character.data.pos.y].has_entity = false
 			character.data.pos = map.current_coords
 			character.position.x = (map.get_current_tile().position.x - 1.25)
 			character.position.z = (map.get_current_tile().position.z - .5)
+			map.get_current_tile().has_entity = true
 		map.reset_tiles()
 		map.draw_zone(character.data.pos, character.data.movement_range)
 		get_tree().current_scene.next_turn()
